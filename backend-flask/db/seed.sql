f9728da767e46c4bafdc32238a10068703965341
Javad Farshchi <mjavad.farshchi@gmail.com>
2024-03-18 04:45:34 +0000

    implement dynamodb into our application

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,10 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, email, cognito_user_id)
+INSERT INTO public.users (display_name,  email, handle,cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'andrew@exampro.co', 'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'bayko@exampro.co', 'MOCK');
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' , 'MOCK'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,  'MOCK'),
+  ('Londo Mollari','Londo@exampro.co', 'Londo' ,  'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
