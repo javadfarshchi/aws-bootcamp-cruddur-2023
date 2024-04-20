966b69def0b6b3b25c887f02ee454b3ef3074561
Javad Farshchi <mjavad.farshchi@gmail.com>
2024-04-20 15:59:12 +0000

    fix expiring token

--- frontend-react-js/src/components/MessageGroupNewItem.js
+++ frontend-react-js/src/components/MessageGroupNewItem.js
@@ -1,5 +1,6 @@
 import './MessageGroupItem.css';
 import { Link } from "react-router-dom";
+import {checkAuth, getAccessToken} from '../lib/CheckAuth';
 
 export default function MessageGroupNewItem(props) {
   return (
